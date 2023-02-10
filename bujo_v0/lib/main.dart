1df0e831bc699a5cd60b29b2e2f8d33bd6b361e8
owenowenisme <mses010108@gmail.com>
2023-02-11 02:03:48 +0800

    firebase configure

--- bujo_v0/lib/main.dart
+++ bujo_v0/lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:bujo_v0/pages/map.dart';
+import 'firebase_options.dart';
 
 
 void main() {
+  Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const Bujoapp());
 }
 
 class Bujoapp extends StatelessWidget {
+  
   const Bujoapp({super.key});
   @override
   Widget build(BuildContext context) {
